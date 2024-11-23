ac8e4ce54387207333e7eab664c42968184c9e75
SHENZA <shenza@oocl.com>
2024-11-24 01:09:08 +0800

    [parkingLot]test: test should_return_car_when_fetching_given_parking_lot_and_ticket

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,8 +1,19 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
 
+    private final Map<Ticket, Car> parkingRecords = new HashMap<>();
+
     public Ticket park(Car car){
-        return new Ticket();
+        Ticket ticket = new Ticket();
+        parkingRecords.put(ticket, car);
+        return ticket;
+    }
+
+    public Car fetch(Ticket ticket){
+        return parkingRecords.remove(ticket);
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -15,5 +16,17 @@ public class ParkingLotTest {
         Ticket ticket = parkingLot.park(car);
         //then
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_car_when_fetching_given_parking_lot_and_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Ticket ticket = parkingLot.park(car);
+        //when
+        Car fetchedCar = parkingLot.fetch(ticket);
+        //then
+        assertEquals(car,fetchedCar);
     }
 }
