0a17ee3bbaf91eef9a837af34ee0483a6da13c92
SHENZA <shenza@oocl.com>
2024-11-24 03:53:20 +0800

    [parkingLot-story6]test:test should_print_error_when_super_smart_parking_boy_has_two_parking_lot_given_error_ticket

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static com.parkinglot.constant.ErrorConstant.UNRECOGNIZED_PARKING_TICKET_ERROR_MSG;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -68,6 +70,22 @@ public class SuperSmartParkingBoyTest {
         // Then
         assertEquals(firstCar, firstFetch);
         assertEquals(secondCar, secondFetch);
+    }
+
+    @Test
+    public void should_print_error_when_super_smart_parking_boy_has_two_parking_lot_given_error_ticket() {
+        // Given
+        SmartParkingBoy boy = new SmartParkingBoy();
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot seconedParkingLot = new ParkingLot();
+        boy.workInParkingLot(firstParkingLot);
+        boy.workInParkingLot(seconedParkingLot);
+        Car firstCar = new Car();
+        // When
+        Ticket ticket = boy.park(firstCar);
+        Car fetch = boy.fetch(new Ticket());
+        // Then
+        assertThat(systemOut()).contains(UNRECOGNIZED_PARKING_TICKET_ERROR_MSG);
     }
 
 
