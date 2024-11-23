3cd3ac40696d1231b0aaebfd34c88e2c25f3a099
SHENZA <shenza@oocl.com>
2024-11-24 03:50:38 +0800

    [parkingLot-story6]test:test should_return_park_car_in_first_parking_lot_when_super_smart_parking_boy_fetch_given_parking_boy_given_has_two_parking_lot_given_same_number_empty

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -1,11 +1,13 @@
 package com.parkinglot;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class SuperSmartParkingBoyTest {
 
@@ -14,7 +16,22 @@ public class SuperSmartParkingBoyTest {
     public void setup() {
         System.setOut(new PrintStream(outContent));
     }
-
+    @Test
+    public void should_return_park_car_in_first_parking_lot_when_super_smart_parking_boy_fetch_given_parking_boy_given_has_two_parking_lot_given_same_number_empty() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        SmartParkingBoy boy = new SmartParkingBoy();
+        boy.workInParkingLot(parkingLot1);
+        boy.workInParkingLot(parkingLot2);
+        Car car = new Car();
+        // When
+        Ticket ticket = boy.park(car);
+        // Then
+        Car fetchedCar1 = parkingLot1.fetch(ticket);
+        assertEquals(car,fetchedCar1);
+        assertFalse(parkingLot2.existTicket(ticket));
+    }
 
     private String systemOut() {
         return outContent.toString();
