bfadf32747de391ed46f31f862d1d2ee374bb42f
SHENZA <shenza@oocl.com>
2024-11-24 01:10:30 +0800

    [parkingLot]test: test should_return_right_car_when_fetch_given_two_ticket

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -29,4 +29,20 @@ public class ParkingLotTest {
         //then
         assertEquals(car,fetchedCar);
     }
+
+    @Test
+    public void should_return_right_car_when_fetch_given_two_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        Car car2 = new Car();
+        Ticket ticket = parkingLot.park(car);
+        Ticket ticket2 = parkingLot.park(car2);
+        //when
+        Car fetchedCar = parkingLot.fetch(ticket);
+        Car fetchedCar2 = parkingLot.fetch(ticket2);
+        //then
+        assertEquals(car,fetchedCar);
+        assertEquals(car2,fetchedCar2);
+    }
 }
