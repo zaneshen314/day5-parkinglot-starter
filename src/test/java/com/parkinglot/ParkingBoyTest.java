b1ff54333eda4aec4dc940fd5c36c5954156ddeb
SHENZA <shenza@oocl.com>
2024-11-24 01:48:11 +0800

    [parkingLot-story3]test: test should_print_error_message_when_parking_boy_fetch_given_wrong_ticket

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -1,11 +1,24 @@
 package com.parkinglot;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static com.parkinglot.constant.ErrorConstant.NO_AVAILABLE_POSITION_ERROR_MSG;
+import static com.parkinglot.constant.ErrorConstant.UNRECOGNIZED_PARKING_TICKET_ERROR_MSG;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
+
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @BeforeEach
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
     public void should_return_ticket_when_parking_boy_park_given_a_car() {
@@ -43,5 +56,22 @@ public class ParkingBoyTest {
         //then
         assertEquals(car,fetchedCar);
         assertEquals(car2,fetchedCar2);
+    }
+
+    @Test
+    public void should_print_error_message_when_parking_boy_fetch_given_wrong_ticket(){
+        // Given
+        ParkingBoy boy = new ParkingBoy();
+        Car car = new Car();
+        Ticket ticket = new Ticket();
+        // When
+        Ticket ignore = boy.park(car);
+        Car fetch = boy.fetch(ticket);
+        // Then
+        assertThat(systemOut()).contains(UNRECOGNIZED_PARKING_TICKET_ERROR_MSG);
+    }
+
+    private String systemOut() {
+        return outContent.toString();
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -135,6 +135,4 @@ public class ParkingLotTest {
     private String systemOut() {
         return outContent.toString();
     }
-
-
 }
